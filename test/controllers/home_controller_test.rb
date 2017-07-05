2f254e0b941259759b355838e1c97785ebdba44c
Dan Thiffault <dan.thiffault@bookbub.com>
2017-07-04 22:34:20 -0400

    Bootstrap layout

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,20 +6,20 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should load monitor by default" do
     get root_url
     assert_response :success
-    assert_select '.title', 'Monitor'
+    assert_select '.navbar-brand', 'Water Processing - Monitor'
   end
 
   test "should show the operator page to operators" do
     sign_in User.where(username: 'operator').first
 
     get root_url
-    assert_select '.title', 'Operator'
+    assert_select '.navbar-brand', 'Water Processing - Operator'
   end
 
   test "should show the admin page to admins" do
     sign_in User.where(username: 'admin').first
 
     get root_url
-    assert_select '.title', 'Administrator'
+    assert_select '.navbar-brand', 'Water Processing - Administrator'
   end
 end
