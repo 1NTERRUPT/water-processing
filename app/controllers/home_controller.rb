94030e47e4520fea8ed4e5449c48607dba8fd871
Dan Thiffault <dan.thiffault@bookbub.com>
2017-08-28 21:04:20 -0400

    Allow operators to control pumps

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,28 @@
 class HomeController < ApplicationController
   def controls
     @pump_status = PumpStatus.last
+    @new_status  = PumpStatus.new
+
+    if @pump_status.operating_at > 0
+      @new_status.operating_at = 0
+      @submit_text = "Disable Pumps"
+    else
+      @new_status.operating_at = 80
+      @submit_text = "Enable Pumps"
+    end
+
     if user_signed_in?
       render :operate
     else
       render :monitor
     end
+  end
+
+  def operate
+    PumpStatusAccumulateJob.perform_now
+    @pump_status = PumpStatus.last
+    @pump_status.update operating_at: params[:pump_status][:operating_at]
+
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  post '/operate', to: 'home#operate'
+
   root to: 'home#controls'
 end
